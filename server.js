d6dff678661514b67410b4a347509938a77bac6a
Lydie Danet <danet.lydie@gmail.com>
2017-03-22 16:42:17 +1300

    Change indentation width

--- server.js
+++ server.js
@@ -5,24 +5,24 @@ const port = (process.env.PORT || 3000)
 app.use(express.static(__dirname + '/www'));
 
 if (process.env.NODE_ENV !== 'production') {
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpack = require('webpack');
-  const webpackConfig = require('./webpack.config.js');
-  const compiler = webpack(webpackConfig);
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpack = require('webpack');
+    const webpackConfig = require('./webpack.config.js');
+    const compiler = webpack(webpackConfig);
 
-  app.use(webpackDevMiddleware(compiler, {
-    hot: true,
-    filename: 'bundle.js',
-    publicPath: '/',
-    stats: {
-      colors: true,
-    },
-    historyApiFallback: true,
-  }));
+    app.use(webpackDevMiddleware(compiler, {
+        hot: true,
+        filename: 'bundle.js',
+        publicPath: '/',
+        stats: {
+            colors: true,
+        },
+        historyApiFallback: true,
+    }));
 }
 
 const server = app.listen(port, function() {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log('App listening at http://%s:%s', host, port);
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log('App listening at http://%s:%s', host, port);
 });
