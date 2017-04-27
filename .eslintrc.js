c8a81cc8b5b1c9e76cea2557205719049657bee5
Lydie Danet <danet.lydie@gmail.com>
2017-04-27 21:58:30 +1200

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "indent": ["error", 4],
+        "react/jsx-indent": ["error", 4],
+        "react/prop-types": "off" //not sure about them yet
+    }
+};
