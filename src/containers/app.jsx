538a521a35812e7eb3d5583b465b9476a3b84cfc
Lydie Danet <danet.lydie@gmail.com>
2017-05-05 18:22:01 +1200

    Add and display chatroom component

--- src/containers/app.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './app.css';
-
-function Hello() {
-    return <h1 className={styles.title}>Hello world</h1>;
-}
-
-export default Hello;
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader'
+import { AppContainer } from 'react-hot-loader';
 
-import App from './containers/app';
+import ChatRoom from './components/chatroom/room';
 
-const render = Component => {
+const DUMMY_MESSAGES = [
+    {
+        sender: 'server',
+        content: 'Welcome on Depict!',
+    },
+    {
+        sender: 'Tom',
+        content: 'Hi there!',
+    },
+    {
+        sender: 'Eva',
+        content: 'Hello',
+    },
+];
+
+const render = (Chat) => {
     ReactDOM.render(
         <AppContainer>
-            <Component />
+            <Chat messages={DUMMY_MESSAGES} />
         </AppContainer>,
         document.getElementById('root'),
     );
 };
 
-render(App);
+render(ChatRoom);
 
 if (module.hot) {
-    module.hot.accept('./containers/app', () => { render(App); });
+    module.hot.accept('./containers/app', () => { render(ChatRoom); });
 }
