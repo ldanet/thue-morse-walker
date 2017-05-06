c77418b3a90be7befaa052bec59bfbdf391e10fd
Lydie Danet <danet.lydie@gmail.com>
2017-05-06 14:21:24 +1200

    Move message component file in discussion

--- src/components/chatroom/discussion/discussion.jsx
+++ src/components/chatroom/discussion/discussion.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './discussion.css';
-import Message from './../message/message';
+import Message from './message/message';
 
 export default function Discussion(props) {
     const messages = props.messages.map(message => <Message message={message} />);
