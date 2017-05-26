c0cfab02870a9cd20823ea2459b4a2aa0f8c785c
Lydie Danet <danet.lydie@gmail.com>
2017-05-26 19:18:36 +1200

    Add modifying rules functionality

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -6,6 +6,10 @@ import styles from './layout.css';
 export default class Layout extends Component {
     constructor(props) {
         super(props);
+        this.handleStepChange = this.handleStepChange.bind(this);
+        this.handleRotationChange = this.handleRotationChange.bind(this);
+        this.handleDeleteRule = this.handleDeleteRule.bind(this);
+        this.handleAddRule = this.handleAddRule.bind(this);
         this.state = {
             options: {
                 delay: 0,
@@ -24,22 +28,49 @@ export default class Layout extends Component {
         };
     }
 
+    handleStepChange(index, e) {
+        const rules = [...this.state.rules];
+        rules[index].step = e.target.checked;
+        this.setState({ rules });
+    }
+
+    handleRotationChange(index, e) {
+        const rules = [...this.state.rules];
+        rules[index].rotation = parseInt(e.target.value, 10);
+        this.setState({ rules });
+    }
+
+    handleDeleteRule(index) {
+        console.log(index);
+        const rules = [...this.state.rules];
+        rules.splice(index, 1);
+        this.setState({ rules });
+    }
+
+    handleAddRule() {
+        const rules = [...this.state.rules];
+        const newRule = Object.assign({}, rules[rules.length - 1]); // Duplicate last rule
+        rules.push(newRule);
+        this.setState({ rules });
+    }
+
     render() {
         const ruleForms = this.state.rules.map((rule, index) => (
             <Rule
                 key={index}
-                id={index}
+                index={index}
                 ruleSet={rule}
                 handleStepChange={this.handleStepChange}
                 handleRotationChange={this.handleRotationChange}
+                handleDeleteRule={this.handleDeleteRule}
             />
         ));
         return (
             <div className={styles.container}>
                 <Canvas options={this.state.options} rules={this.state.rules} />
                 {ruleForms}
+                <button title="Add rule" onClick={this.handleAddRule}>+</button>
             </div>
         );
     }
-
 }
--- src/components/rule/rule.jsx
+++ src/components/rule/rule.jsx
@@ -3,29 +3,32 @@ import styles from './rule.css';
 
 export default function rule(props) {
     return (<div className={styles.rule}>
-        <div className={styles.id}>{props.id}</div>
+        <div className={styles.index}>{props.index}</div>
         <div className={styles.properties}>
             <div className={styles.property}>
-                <label htmlFor={`angle${props.id}`}>Rotate</label>
+                <label htmlFor={`angle${props.index}`}>Rotate </label>
                 <input
                     type="number"
-                    id={`angle${props.id}`}
-                    onChange={props.handleAngleChange}
+                    id={`angle${props.index}`}
+                    onChange={e => props.handleRotationChange(props.index, e)}
                     value={props.ruleSet.rotation}
                 /> degrees
             </div>
             <div className={styles.property}>
                 <input
                     type="checkbox"
-                    id={`step${props.id}`}
-                    onChange={props.handleStepChange}
+                    id={`step${props.index}`}
+                    onChange={e => props.handleStepChange(props.index, e)}
                     checked={props.ruleSet.step}
                 />
-                <label htmlFor={`step${props.id}`}>Step</label>
+                <label htmlFor={`step${props.index}`}>Step</label>
             </div>
         </div>
         <div className={styles.delete}>
-            <button title="Delete rule" onClick={props.handleDeleteRule}>X</button>
+            <button
+                title="Delete rule"
+                onClick={() => props.handleDeleteRule(props.index)}
+            >X</button>
         </div>
     </div>);
 }
